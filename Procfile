0c39328d5abd2f31e75f6efd6009229fe6cd7715
Samot428 <tomasfantomastomi@gmail.com>
2026-01-03 21:34:13 +0100

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 # Procfile for Heroku, Railway, or other platforms
-web: cd DANCE_CALENDAR_WEB && python manage.py migrate && gunicorn DANCE_CALENDAR_WEB.wsgi:application --bind 0.0.0.0:$PORT
+web: python DANCE_CALENDAR_WEB/manage.py migrate && gunicorn --chdir DANCE_CALENDAR_WEB DANCE_CALENDAR_WEB.wsgi:application --bind 0.0.0.0:$PORT
--- build.sh
+++ build.sh
@@ -5,15 +5,10 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Setting up Django..."
-# Set Python path to include the Django project directory
-export PYTHONPATH="${PYTHONPATH}:${PWD}/DANCE_CALENDAR_WEB"
-
 echo "Collecting static files..."
-cd DANCE_CALENDAR_WEB
-python manage.py collectstatic --no-input
+python DANCE_CALENDAR_WEB/manage.py collectstatic --no-input
 
 echo "Running database migrations..."
-python manage.py migrate
+python DANCE_CALENDAR_WEB/manage.py migrate
 
 echo "Build complete!"
